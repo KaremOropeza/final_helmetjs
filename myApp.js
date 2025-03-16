9264325b8b69946e13a736b97373800fc73dbd3a
Karem Oropeza <karemoropezayan@gmail.com>
2025-03-15 23:20:43 -0600

    HidePoweredBy

--- myApp.js
+++ myApp.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const helmet =require("helmet")
 const app = express();
 
 
@@ -50,6 +51,8 @@ const app = express();
 module.exports = app;
 const api = require('./server.js');
 app.use(express.static('public'));
+app.use(helmet.hidePoweredBy());
+
 app.disable('strict-transport-security');
 app.use('/_api', api);
 app.get("/", function (request, response) {
