78661200df2a4e471264ade279bd3851cd51872c
Karem Oropeza <karemoropezayan@gmail.com>
2025-04-07 19:39:26 -0600

    Comentarios en myApps.js

--- myApp.js
+++ myApp.js
@@ -3,20 +3,27 @@ const helmet =require("helmet");
 const app = express();
 
 
-
+// 1. Oculta la cabecera X-Powered-By, que suele indicar que usas Express, lo que podría dar pistas a atacantes.
 // app.use(helmet.hidePoweredBy());
+// 2. Evita que tu app sea incrustada en un iframe (lo cual previene ataques tipo clickjacking).
 // app.use(helmet.frameguard({action: 'deny'}));
+// 3. Ayuda a prevenir inyecciones de scripts maliciosos
 // app.use(helmet.xssFilter());
+// 4.  Evita que navegadores infieran el tipo MIME del contenido, protegiendo de ataques por contenido malicioso
 // app.use(helmet.noSniff());
+// 5. Previene que IE ejecute archivos como HTML no confiables.
 // app.use(helmet.ieNoOpen());
 
+//Solicite a los navegadores que accedan al sitio solo a través de HTTPS
 // const timeInSeconds = 90 * 24 * 60 * 60;
 // app.use(helmet.hsts({maxAge: timeInSeconds, force: true}));
 
+//Desactiva la búsqueda previa de DNS
 // app.use(helmet.dnsPrefetchControl());
-
+//Desactiva el almacenamiento en caché del navegador para prevenir que se almacene información sensible
 // app.use(helmet.noCache());
 
+//app.use(helmet())Incluirá automáticamente todo el middleware mencionado anteriormente, excepto noCache(), y contentSecurityPolicy(), por lo cual se habilitan.
 app.use(helmet({
   contentSecurityPolicy: {
     directives: {
